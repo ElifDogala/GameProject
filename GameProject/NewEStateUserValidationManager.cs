adab51af526059494d5a5b71039403c58e897fab
ElifDogala <elif.dogala@hotmail.com>
2022-06-04 13:05:18 +0300

    Add project files.

---
+++ GameProject/NewEStateUserValidationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class NewEStateUserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            return true;
+
+        }
+    }
+}
